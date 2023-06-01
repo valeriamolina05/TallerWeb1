5743beb2794600dbb355ac49ca6a26d52db8446d
ValeriaM <valemolinac05@gmail.com>
2023-06-01 16:33:37 -0500

    Tercer punto TallerWeb

--- script.js
+++ script.js
@@ -28,6 +28,20 @@ while(repetir){
             let area = pi * radio * radio; 
             console.log("El área de la circunferencia es: " + area);
             break;
+        case 3:
+            contador[2]++;
+            let lado1 = parseFloat(prompt("Ingrese el primer lado del triángulo:"));
+            let lado2 = parseFloat(prompt("Ingrese el segundo lado del triángulo:"));
+            let lado3 = parseFloat(prompt("Ingrese el tercer lado del triángulo:"));
+
+            if (lado1 === lado2 && lado2 === lado3) {
+                console.log("El triángulo es equilátero.");
+              } else if (lado1 === lado2 || lado1 === lado3 || lado2 === lado3) {
+                console.log("El triángulo es isósceles.");
+              } else {
+                console.log("El triángulo es escaleno.");
+              }
+            break;
 
     }
     }
